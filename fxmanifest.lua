cde62685de588fe365d1803183f65d27f5feff34
Shailane <107360148+Shailane1591@users.noreply.github.com>
2022-06-14 20:28:42 +0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Blumlaut'
+description 'a free and open anticheat.'
+
+
+server_scripts {
+	"config_ac.lua",
+	"ac_s.lua"
+} 
+
+client_scripts {
+	"config.lua",
+	"ac_c.lua"
+}
